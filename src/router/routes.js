86993249152ba88340e22895ecddfb64b45c8547
Alan Costa <alanpc7@gmail.com>
2024-05-05 19:54:42 -0300

    geral

--- src/router/routes.js
+++ src/router/routes.js
@@ -9,7 +9,6 @@ const routes = [
       { path: 'contact', name: 'contact', component: () => import('pages/Contact.vue') },
       { path: 'gallery', name: 'gallery', component: () => import('pages/Gallery.vue') },
       { path: 'gallery/:id', name: 'listaimg', component: () => import('pages/ListaImg.vue') },
-
     ]
   },
 
